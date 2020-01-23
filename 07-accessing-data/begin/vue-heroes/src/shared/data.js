d5ce66feaf9e15f3435c8bd80af84991dfdb3dde
Morten Lundenes <morten.lundenes@bouvet.no>
2020-01-23 15:03:17 +0100

    add data file

--- 07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ 07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -2,14 +2,36 @@ import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
 import * as axios from 'axios';
 import { API } from './config';
+
 const getHeroes = async function() {
-  const response = await axios.get(`${API}/heroes.json`);
-  const heroes = response.data.map(h => {
-    h.originDate = format(h.originDate, inputDateFormat);
-    return h;
-  });
-  return heroes;
+  try {
+    const response = await axios.get(`${API}/heroes.json`);
+    const data = parseList(response);
+    const heroes = data.map(h => {
+      h.originDate = format(h.originDate, inputDateFormat);
+      return h;
+    });
+    return heroes;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
+
+const parseList = response => {
+  if (response.status !== 200) {
+    throw Error(response.message);
+  }
+  if (!response.data) {
+    return [];
+  }
+  let list = response.data;
+  if (typeof list !== 'object') {
+    list = [];
+  }
+  return list;
+};
+
 export const data = {
   getHeroes,
 };
